b62112587242014f69ef02726f3b47124a6928a0
Metin-3 <7kp0v0e@code.edu.az>
2024-11-21 23:56:57 +0400

    add product detail price calculate Redux Toolkit

--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -1,49 +1,93 @@
-import Title from '../../components/ui/Title'
-import Image from 'next/image'
-import React from 'react'
+import Title from '../../components/ui/Title';
+import Image from 'next/image';
+import { useState } from 'react';
+
+const itemsExtra = [
+    {
+        id: 1,
+        name: "Extra 1",
+        price: 1,
+    },
+    {
+        id: 2,
+        name: "Extra 2",
+        price: 2,
+    },
+    {
+        id: 3,
+        name: "Extra 3",
+        price: 3,
+    },
+]
 
 const index = () => {
+
+    const [prices, setPrices] = useState([10, 20, 30]);
+    const [price, setPrice] = useState(prices[0]);
+    const [size, setSize] = useState(0);
+    const [extraItems, setExtraItems] = useState(itemsExtra);
+    const [extras, setExtras] = useState([]);
+
+    const handleSize = (sizeIndex) => {
+        const difference = prices[sizeIndex] - prices[size];
+        setSize(sizeIndex);
+        changePrice(difference);
+    }
+
+    const changePrice = (number) => {
+        setPrice(price + number)
+    }
+
+    const handleChange = (e, item) => {
+        const checked = e.target.checked;
+
+        if (checked) {
+            changePrice(item.price)
+            setExtras([...extras, item])
+        } else {
+            changePrice(-item.price)
+            setExtras(extras.filter((extra) => extra.id !== item.id));
+        }
+    }
+
     return (
-        <div className='flex flex-wrap items-center justify-center h-screen gap-20 py-20 '>
-            <div className='relative  md:flex-1 w-[50%] h-[50%] md:w-[80%] md:h-[80%] mx-20'>
+        <div className='flex flex-wrap items-center justify-center md:h-[calc(100vh_-_88px)] gap-5 py-20 '>
+            <div className='relative  md:flex-1 w-36 h-36 md:w-[80%] md:h-[80%] mx-auto'>
                 <Image src="/images/f1.png" alt='' layout='fill' objectFit='contain' />
             </div>
             <div className='md:flex-1 md:block flex flex-col items-center py-6'>
                 <Title addClass="text-6xl">Good Pizza</Title>
-                <span className='text-primary text-2xl font-bold underline underline-offset-8 my-4 inline-block'>10 $</span>
+                <span className='text-primary text-2xl font-bold underline underline-offset-8 my-4 inline-block'>
+                    {price} $
+                </span>
                 <p className='text-sm my-4 md:pr-24 pr-0 text-center md:text-start'>
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores explicabo labore molestiae fugiat at sed?
                 </p>
                 <div>
                     <h4 className='text-xl font-bold'>Choose the size</h4>
                     <div className='flex items-center gap-x-20'>
-                        <div className='relative w-8 h-8'>
+                        <div className='relative w-8 h-8 cursor-pointer' onClick={() => handleSize(0)}>
                             <Image src="/images/size.png" alt='' layout='fill' />
                             <span className='absolute top-0 -right-7 text-xs bg-primary rounded-full px-[5px] font-medium'>Small</span>
                         </div>
-                        <div className='relative w-12 h-12'>
+                        <div className='relative w-12 h-12 cursor-pointer' onClick={() => handleSize(1)}>
                             <Image src="/images/size.png" alt='' layout='fill' />
                             <span className='absolute top-0 -right-8 text-xs bg-primary rounded-full px-[5px] font-medium'>Medium</span>
                         </div>
-                        <div className='relative w-16 h-16'>
+                        <div className='relative w-16 h-16 cursor-pointer' onClick={() => handleSize(2)}>
                             <Image src="/images/size.png" alt='' layout='fill' />
                             <span className='absolute top-0 -right-4 text-xs bg-primary rounded-full px-[5px] font-medium'>Large</span>
                         </div>
                     </div>
                 </div>
                 <div className='flex gap-x-6 my-6'>
-                    <label className='flex items-center gap-x-1'>
-                        <input type="checkbox" className='w-5 h-5 accent-primary' />
-                        <span className='text-sm font-semibold'>Ketcup</span>
-                    </label>
-                    <label className='flex items-center gap-x-1'>
-                        <input type="checkbox" className='w-5 h-5 accent-primary' />
-                        <span className='text-sm font-semibold'>Mayonez</span>
-                    </label>
-                    <label className='flex items-center gap-x-1'>
-                        <input type="checkbox" className='w-5 h-5 accent-primary' />
-                        <span className='text-sm font-semibold'>Aci sos</span>
-                    </label>
+                    {extraItems.map((item) => (
+                        <label className='flex items-center gap-x-1' key={item.id}>
+                            <input type="checkbox" className='w-5 h-5 accent-primary' onChange={(e) => handleChange(e, item)} />
+                            <span className='text-sm font-semibold'>{item.name}</span>
+                        </label>
+                    ))}
+
                 </div>
                 <button className='btn-primary mt-6'>Add To Card</button>
             </div>
