e8f8e5fd22fe5baa608f7a087e6ecbcd68157ac7
Metin-3 <7kp0v0e@code.edu.az>
2024-11-23 14:58:42 +0400

    create users model

---
+++ models/User.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            require: true,
+        },
+        email: {
+            type: String,
+            require: true,
+        },
+        phoneNumber: {
+            type: String,
+            require: true,
+        },
+        adress: {
+            type: String,
+            require: true,
+        },
+        job: {
+            type: String,
+            require: true,
+        },
+        bio: {
+            type: String,
+            require: true,
+        },
+        password: {
+            type: String,
+            require: true,
+        },
+        confirmPassword: {
+            type: String,
+            require: true,
+        },
+        emailVerified: {
+            type: String,
+            default: null,
+        }
+    },
+    {
+        timestamps: true,
+    },
+);
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
