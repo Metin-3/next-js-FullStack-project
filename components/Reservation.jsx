383ea9d842412e3457d88a67ab1c7b277c418386
Metin-3 <7kp0v0e@code.edu.az>
2024-11-23 17:24:07 +0400

    auth with GitHub

--- components/Reservation.jsx
+++ components/Reservation.jsx
@@ -89,7 +89,7 @@ const Reservation = () => {
                 <div className='lg:flex-1 w-full'>
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387191.0361190753!2d-74.30933632161008!3d40.69753995481267!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2saz!4v1732046123380!5m2!1sen!2saz"
-                        allowfullscreen=""
+                        allowFullScreen=""
                         loading="lazy"
                         referrerPolicy="no-referrer" 
                         // referrerpolicy="no-referrer-when-downgrade"
--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -3,10 +3,14 @@ import Input from '../../components/form/Input'
 import Title from '../../components/ui/Title'
 import { loginSchema } from '../../schema/login';
 import Link from 'next/link';
+import { useSession, signIn } from "next-auth/react"
 
 
 const Login = () => {
 
+    const { data: session } = useSession();
+
+    console.log(session)
     const onSubmit = async (values, actions) => {
         await new Promise((resolve) => setTimeout(resolve, 4000));
         actions.resetForm();
@@ -56,8 +60,8 @@ const Login = () => {
                     ))}
                 </div>
                 <div className='flex flex-col w-full gap-y-3 mt-6'>
-                    <button className="btn-primary font-bold">Login</button>
-                    <button className="btn-primary !bg-secondary font-bold">
+                    <button className="btn-primary font-bold" type="submit">Login</button>
+                    <button className="btn-primary !bg-secondary font-bold" type='button' onClick={() => signIn("github")}>
                         <i className="fa-brands fa-github"></i>  GitHub
                     </button>
                     <Link href="/auth/register">
