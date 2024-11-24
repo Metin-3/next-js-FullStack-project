2b279f70e3c3f4b5cde2769a2e99091fdd13ab37
Metin-3 <7kp0v0e@code.edu.az>
2024-11-24 21:42:44 +0400

    category  DELETE

--- components/admin/Category.jsx
+++ components/admin/Category.jsx
@@ -1,10 +1,49 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Title from '../ui/Title';
 import Input from '../form/Input';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Category = () => {
     const [inputTex, setInputTex] = useState();
     const [categories, setCategories] = useState([]);
+
+
+    useEffect(() => {
+        const getCategories = async () => {
+            try {
+                const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/categories`);
+                setCategories(res?.data);
+            } catch (error) {
+                console.log(error)
+            }
+        };
+        getCategories();
+    }, []);
+
+
+    const handleCreate = async () => {
+        try {
+            const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/categories`, { title: inputTex });
+            setCategories([...categories, res.data]);
+            setInputTex("");
+            toast.success("Create category successfully");
+        } catch (error) {
+            toast.error("Failed to create category");
+            console.log(error)
+        }
+    };
+
+
+    const handleDelete = async (id) => {
+        try {
+            await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/categories/${id}`);
+            setCategories(categories.filter((cat) => cat._id !== id));
+            toast.success("Delete category successfully")
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <div className="lg:p-8 flex-1 lg:mt-5 mt-0">
@@ -15,18 +54,17 @@ const Category = () => {
                         placeholder="Add a new category..."
                         onChange={(e) => setInputTex(e.target.value)}
                         value={inputTex}
-                    />~~~~
-                    {inputTex}
-                    <button className="btn-primary" onClick={() => {
-                        setCategories([...categories, inputTex])
-                        setInputTex("")
-                    }}>Add</button>
+                    />
+                    <button
+                        className="btn-primary font-bold !text-secondary"
+                        onClick={handleCreate}
+                    >Add</button>
                 </div>
-                <div className='mt-10'>
-                    {categories.map((category, index) => (
-                        <div className='flex justify-between mt-4' key={index}>
-                            <b className='text-xl'>{category}</b>
-                            <button className="btn-primary !bg-danger" onClick={() => setCategories(categories.filter((cat) => cat !== category))}>Delete</button>
+                <div className='mt-10 max-h-[250px] overflow-auto  pb-4'>
+                    {categories.map((category) => (
+                        <div className='flex justify-between mt-4' key={category._id}>
+                            <b className='text-xl'>{category.title}</b>
+                            <button className="btn-primary !bg-danger" onClick={() => handleDelete(category._id)}>Delete</button>
                         </div>
                     ))}
                 </div>
