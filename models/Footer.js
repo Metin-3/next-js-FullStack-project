06a9f9efb68ff4ebd438af97eb9ec87aefbd8ae7
Metin-3 <7kp0v0e@code.edu.az>
2024-12-05 23:05:06 +0400

    created footer model

---
+++ models/Footer.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const FooterSchema = new mongoose.Schema(
+    {
+        location: {
+            type: String,
+            require: true,
+        },
+        email: {
+            type: String,
+            require: true,
+        },
+        phoneNumber: {
+            type: String,
+            require: true,
+        },
+        desc: {
+            type: String,
+            require: true,
+        },
+        socialMedia: {
+            type: [
+                {
+                    icon: { type: String },
+                    link: { type: String },
+                }
+            ],
+        },
+        openigHours: {
+            type: {
+                day: { type: String },
+                hour: { type: String },
+            },
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+export default mongoose.models.Footer || mongoose.model("Footer", FooterSchema);
